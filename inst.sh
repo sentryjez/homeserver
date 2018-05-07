0112ace2137536d50614b1423f5ce28223b30d1c
Jens Merlier <39005150+sentryjez@users.noreply.github.com>
2018-05-07 15:00:02 +0200

    Add files via upload

--- inst.sh
+++ inst.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
+
+apt update && apt upgrade
+
+apt -y install docker.io cockpit cockpit-docker
+
+useradd -M -u 1001 -g 1001 -s /sbin/nologin usenet
+
+/bin/bash ./build/buildrad.sh
+
+/bin/bash ./build/buildson.sh
+
+/bin/bash ./build/buildsab.sh
+
+/bin/bash ./build/buildemb.sh
+
+
+
 
