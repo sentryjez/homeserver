0bb9b266fad8c54f904ab583beedf8141593cf14
Jens Merlier <39005150+sentryjez@users.noreply.github.com>
2018-05-08 15:31:25 +0200

    Update inst.sh

--- inst.sh
+++ inst.sh
@@ -6,12 +6,11 @@
 
 apt update && apt upgrade
 
-
 ######################################################################
 #2.Install the required packages to build our homeserver
 ###		Instal Docker and the Cockpit-webgui
-apt -y install docker.io cockpit cockpit-docker
 
+apt -y install docker.io cockpit cockpit-docker
 
 ######################################################################
 #3.Media-stack
@@ -20,25 +19,19 @@ apt -y install docker.io cockpit cockpit-docker
 
 useradd -M -u 1001 -g 1001 -s /sbin/nologin usenet
 
-
-
 ##	3.2.Build the Docker-Containers and get them started
-###		Script for "Radarr"-container
 
+###		Script for "Radarr"-container
 /bin/bash ./build/buildrad.sh
 
 ###		Script for "Sonarr"-container
-
 /bin/bash ./build/buildson.sh
 
 ###		Script for "Sabnzbd"-container
-
 /bin/bash ./build/buildsab.sh
 
 ###		Script for "Emby"-container
-
 /bin/bash ./build/buildemb.sh
-
 
 ######################################################################
 #99.Post-install
@@ -51,38 +44,32 @@ function disp {
 }
 
 ###		Cockpit-webgui 
-
 app=Cockpit-webgui
 port=9090
 disp
 
 ###		Radarr-webgui
-
 app=Radarr
 port=7878
 disp
 
 ###		Sonarr-webgui
-
 app=Sonarr
 port=8989
 disp
 
 ###		Sabnzbd-webgui
-
 app=Sabnzbd
 port=8080
 disp
 
 ###		Emby-webgui
-
 app=Emby
 port=8096
 disp
 
-
-
 ##	99.99.Reboot
+
 reboot now
 
 
