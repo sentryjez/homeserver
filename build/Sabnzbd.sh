42497fd209e7ab25c5dd944d2046d14942355c53
Jens Merlier <39005150+sentryjez@users.noreply.github.com>
2018-05-11 20:43:11 +0200

    Create Sabnzbd.sh

---
+++ build/Sabnzbd.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+docker pull sabnzbd/sabnzbd:latest
+
+mkdir -p /apps/sabnzbd
+chown -R usenet /apps/sabnzbd
+
+docker create \
+--name=sabnzbd \
+-v /apps/sabnzbd:/datadir \
+-v /storage/downloads:/downloads \
+-v /etc/localtime:/etc/localtime:ro \
+-e GID=1001 -e UID=1001 \
+-p 8080:8080 \
+sabnzbd/sabnzbd
+
+docker start sabnzbd
